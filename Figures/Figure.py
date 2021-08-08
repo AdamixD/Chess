00a7733e69bf37e2bb31e6e582f2bdf17e06016c
Yaroslav <garbar.jarek@gmail.com>
2021-08-08 10:48:11 +0300

    modified function check_next_field()

--- Figures/Figure.py
+++ Figures/Figure.py
@@ -23,10 +23,13 @@ class Figure:
     def set_position(self, new_position):
         self._position = new_position
 
-    def check_next_field(self, board):
+    # We use this function for function is_check() in class Board
+    def check_next_field_simple(self, board):
         array = []
         x = self._position[0]
         y = self._position[1]
+
+        board_array = board.get_array()
 
         for combination in self._combinations:
             new_x = x + combination[0]
@@ -34,23 +37,67 @@ class Figure:
 
             if self._long_step_move:
                 while new_x >= 0 and new_x < 8 and new_y >= 0 and new_y < 8:
-                    if board[new_x][new_y].get_name() == " ":
+                    if board_array[new_x][new_y].get_name() == " ":
                         array.append([new_x, new_y])
                     else:
-                        if board[new_x][new_y].get_team() != self._team:
+                        if board_array[new_x][new_y].get_team() != self._team:
                             array.append([new_x, new_y])
                         break
                     new_x += combination[0]
                     new_y += combination[1]
             else:
                 if new_x >= 0 and new_x < 8 and new_y >= 0 and new_y < 8:
-                    if board[new_x][new_y].get_name() == " ":
+                    if board_array[new_x][new_y].get_name() == " ":
                         array.append([new_x, new_y])
                     else:
-                        if board[new_x][new_y].get_team() != self._team:
+                        if board_array[new_x][new_y].get_team() != self._team:
                             array.append([new_x, new_y])
         return array
 
+    def check_if_check(self, board, new_position):
+        board_copy = board
+        board_copy.change_figure_position(self._position, new_position)
+        return board_copy.is_check(self._team)
+
+    # This function is used for figures
+    def check_next_field(self, board):
+        array = []
+        x = self._position[0]
+        y = self._position[1]
+
+        board_array = board.get_array()
+        
+        for combination in self._combinations:
+            new_x = x + combination[0]
+            new_y = y + combination[1]
+
+            if self._long_step_move:
+                while new_x >= 0 and new_x < 8 and new_y >= 0 and new_y < 8:
+                    if board_array[new_x][new_y].get_name() == " ":
+                        if self.check_if_check(board, [new_x, new_y]):
+                            continue
+                        array.append([new_x, new_y])
+                    else:
+                        if board_array[new_x][new_y].get_team() != self._team:
+                            if self.check_if_check(board, [new_x, new_y]):
+                                continue
+                            array.append([new_x, new_y])
+                        break
+                    new_x += combination[0]
+                    new_y += combination[1]
+            else:
+                if new_x >= 0 and new_x < 8 and new_y >= 0 and new_y < 8:
+                    if board_array[new_x][new_y].get_name() == " ":
+                        if self.check_if_check(board, [new_x, new_y]):
+                            continue
+                        array.append([new_x, new_y])
+                    else:
+                        if board_array[new_x][new_y].get_team() != self._team:
+                            if self.check_if_check(board, [new_x, new_y]):
+                                continue
+                            array.append([new_x, new_y])
+        return array
+    
     def __str__(self):
         return self._name
 
