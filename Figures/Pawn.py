ad11ce1bc6ae8fc323ef14ab64927f683e6c9905
Yaroslav <garbar.jarek@gmail.com>
2021-08-09 13:29:36 +0300

    add check_next_field_simple() to class Pawn

--- Figures/Pawn.py
+++ Figures/Pawn.py
@@ -12,6 +12,36 @@ class Pawn(Figure):
     def set_is_moved(self, is_moved):
         self._is_moved = is_moved
 
+    def check_next_field_simple(self, board):
+        array = []
+        x = self._position[0]
+        y = self._position[1]
+
+        board_array = board.get_array()
+
+        step = -1
+        if not self._team:
+            step = 1
+
+        new_x = x
+
+        number_of_steps = 2
+        if self._is_moved:
+            number_of_steps = 1
+
+        for i in range(number_of_steps):
+            new_x += step
+            if new_x >= 0 and new_x < 8:
+                if board_array[new_x][y].get_name() == " ":
+                    array.append([new_x, y])
+                if i == 0:
+                    if y+step<8 and y+step>=0 and board_array[new_x][y+step].get_name() != " " and board_array[new_x][y+step].get_team() != self._team:
+                        array.append([new_x, y+step])
+
+                    if y-step>=0 and y-step<8 and board_array[new_x][y-step].get_name() != " " and board_array[new_x][y-step].get_team() != self._team:
+                        array.append([new_x, y-step])
+        return array
+
     def check_next_field(self, board):
         array = []
         x = self._position[0]
